0090296af370c1124fe7a1007f60ae8a1d8b1c57
ngyuki <ngyuki.jp@gmail.com>
2025-06-18 09:24:30 +0900

    SSMパラメータのトークン値をignore_changesに設定

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -3,10 +3,18 @@ resource "aws_ssm_parameter" "feedly_token" {
   name  = local.ssm_parameter_feedly
   type  = "String"
   value = var.feedly_token
+
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
 
 resource "aws_ssm_parameter" "raindrop_token" {
   name  = local.ssm_parameter_raindrop
   type  = "String"
   value = var.raindrop_token
+
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
